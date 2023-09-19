d8e991346180cd539d8308fde9faae6455d13787
Caroline Merkle <carolinemerkle@MacBook-Pro-4.local>
2023-09-19 13:07:01 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
@@ -27,6 +29,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Constants.ShooterConstants;
 
+import frc.robot.subsystems.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -37,13 +40,12 @@ import frc.robot.Constants.ShooterConstants;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
   private final DriveTrain m_DriveTrain = new DriveTrain();
-  private final Joystick m_Joystick = new Joystick(OperatorConstants.JoystickID);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Joystick m_Joystick = new Joystick(OperatorConstants.JoystickID);
+  private final CommandXboxController m_CommandXboxController = new CommandXboxController(1);
+
+  private final Shooter m_Shooter = new Shooter();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -67,26 +69,54 @@ public class RobotContainer {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-    
-    m_XboxController.rightBumper().whileTrue(new StartEndCommand(() -> m_Shooter.ShootCube(ShooterConstants.outerSpeed, ShooterConstants.innerSpeed), () -> m_Shooter.ShootCube(0,0), m_Shooter));
-    m_XboxController.leftBumper().whileTrue(new StartEndCommand(() -> m_Shooter.IntakeCube(ShooterConstants.outerSpeedReversed, ShooterConstants.innerSpeedReversed), () -> m_Shooter.IntakeCube(0, 0), m_Shooter));
-    //m_XboxController.rightBumper().whileTrue(new StartEndCommand(() -> m_Shooter.runShooter(ShooterConstants.innerSpeed), () -> m_Shooter.runShooter(0), m_Shooter));
-    //m_XboxController.leftBumper().whileTrue(new StartEndCommand(() -> m_Shooter.runShooter(ShooterConstants.frontShooterSpeedReverse), () -> m_Shooter.runShooter(0), m_Shooter));
 
-    //m_XboxController.leftTrigger().whileTrue(new StartEndCommand(() -> m_Shooter.runIndex(ShooterConstants.outerSpeed), () -> m_Shooter.runIndex(0), m_Shooter));
-   // m_XboxController.rightTrigger().whileTrue(new StartEndCommand(() -> m_Shooter.runIndex(ShooterConstants.backIndexSpeedReverse), () -> m_Shooter.runIndex(0), m_Shooter));
+    m_CommandXboxController
+        .rightBumper()
+        .whileTrue(
+            new StartEndCommand(
+                () -> m_Shooter.ShootCube(ShooterConstants.outerSpeed, ShooterConstants.innerSpeed),
+                () -> m_Shooter.ShootCube(0, 0),
+                m_Shooter));
+    m_CommandXboxController
+        .leftBumper()
+        .whileTrue(
+            new StartEndCommand(
+                () ->
+                    m_Shooter.IntakeCube(
+                        ShooterConstants.outerSpeedReversed, ShooterConstants.innerSpeedReversed),
+                () -> m_Shooter.IntakeCube(0, 0),
+                m_Shooter));
+    // m_XboxController.rightBumper().whileTrue(new StartEndCommand(() ->
+    // m_Shooter.runShooter(ShooterConstants.innerSpeed), () -> m_Shooter.runShooter(0),
+    // m_Shooter));
+    // m_XboxController.leftBumper().whileTrue(new StartEndCommand(() ->
+    // m_Shooter.runShooter(ShooterConstants.frontShooterSpeedReverse), () ->
+    // m_Shooter.runShooter(0), m_Shooter));
 
+    // m_XboxController.leftTrigger().whileTrue(new StartEndCommand(() ->
+    // m_Shooter.runIndex(ShooterConstants.outerSpeed), () -> m_Shooter.runIndex(0), m_Shooter));
+    // m_XboxController.rightTrigger().whileTrue(new StartEndCommand(() ->
+    // m_Shooter.runIndex(ShooterConstants.backIndexSpeedReverse), () -> m_Shooter.runIndex(0),
+    // m_Shooter));
 
-    m_XboxController.leftTrigger().whileTrue(new StartEndCommand(() -> m_Shooter.IntakeCone(ShooterConstants.innerSpeed, ShooterConstants.outerSpeedReversed), () -> m_Shooter.IntakeCone(0, 0), m_Shooter));
-    m_XboxController.rightTrigger().whileTrue(new StartEndCommand(() -> m_Shooter.ShootCone(ShooterConstants.innerSpeedReversed, ShooterConstants.outerSpeed), () -> m_Shooter.ShootCone(0, 0), m_Shooter));
-
-
-
-    
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_CommandXboxController
+        .leftTrigger()
+        .whileTrue(
+            new StartEndCommand(
+                () ->
+                    m_Shooter.IntakeCone(
+                        ShooterConstants.innerSpeed, ShooterConstants.outerSpeedReversed),
+                () -> m_Shooter.IntakeCone(0, 0),
+                m_Shooter));
+    m_CommandXboxController
+        .rightTrigger()
+        .whileTrue(
+            new StartEndCommand(
+                () ->
+                    m_Shooter.ShootCone(
+                        ShooterConstants.innerSpeedReversed, ShooterConstants.outerSpeed),
+                () -> m_Shooter.ShootCone(0, 0),
+                m_Shooter));
   }
 
   /**
